72bef69152981425fab71ed8ce032981b26cc74c
GAURANG-21 <gaurang2621@gmail.com>
2024-04-16 16:56:05 +0530

    add: Schemas, Controllers and Routes

--- Coursebundlerserver/app.js
+++ Coursebundlerserver/app.js
@@ -1,3 +1,7 @@
-import express from 'express'
+import express from "express";
+import course from "./routes/courseRoutes.js";
+import user from "./routes/userRoutes.js";
 
 export const app = express();
+app.use("/api/v1", course);
+app.use("/api/v1", user);
